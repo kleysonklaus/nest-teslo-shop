ad198738876a684051aee15d74eb8f28935eabe7
kleyson klaus <kleyson.zur44@gmail.com>
2023-09-02 14:38:06 -0500

    142, enlace de uno a muchos y muchos a uno

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "./index";
 
 @Entity()
 export class ProductImage {
@@ -7,4 +8,11 @@ export class ProductImage {
 
     @Column('text')
     url: string;
+
+    @ManyToOne(
+        () => Product,
+        (product) => product.images,
+
+    )
+    product: Product;
 }
