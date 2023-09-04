90b1e36c603a3fd719aafdad55c09b0ebef30675
kleyson klaus <kleyson.zur44@gmail.com>
2023-09-03 21:15:03 -0500

    147, eliminacion por cascada, solucion sin error

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -12,7 +12,9 @@ export class ProductImage {
     @ManyToOne(
         () => Product,
         (product) => product.images,
-
+        {
+            onDelete: "CASCADE"
+        }
     )
     product: Product;
 }
